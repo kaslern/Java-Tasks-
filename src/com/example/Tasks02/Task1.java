179d922e9e4b295ca07c38588e460a3c00a5fa00
kaslern <adixonek@gmail.com>
2026-05-27 19:01:37 +0200

    Task1 classic loop

---
+++ src/com/example/Tasks02/Task1.java
@@ -0,0 +1,9 @@
+package com.example.Tasks02;
+
+public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
