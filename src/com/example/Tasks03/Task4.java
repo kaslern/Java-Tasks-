eecda185f9a1710d3b96863cc92e25c93bd0ee04
kaslern <adixonek@gmail.com>
2026-05-31 18:39:56 +0200

    Task4 array sum

---
+++ src/com/example/Tasks03/Task4.java
@@ -0,0 +1,14 @@
+package com.example.Tasks03;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        int[] numbers = {5, 10, 15, 20, 25};
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++){
+            sum = sum + numbers[i];
+        }
+        System.out.println(sum);
+    }
+}
