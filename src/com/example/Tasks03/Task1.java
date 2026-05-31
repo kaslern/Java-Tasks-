fe3bb5d9d0bba102a9000acc86b9bb68faddd2e8
kaslern <adixonek@gmail.com>
2026-05-31 17:38:48 +0200

    Task1 numbers array

---
+++ src/com/example/Tasks03/Task1.java
@@ -0,0 +1,12 @@
+package com.example.Tasks03;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int[] numbers = {5, 10, 15, 20, 25};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
