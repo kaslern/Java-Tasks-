f69083801f701f5091deafcfd12d25ac0e394692
kaslern <adixonek@gmail.com>
2026-05-31 22:12:44 +0200

    Task10 reveresed array

---
+++ src/com/example/Tasks03/Task10.java
@@ -0,0 +1,13 @@
+package com.example.Tasks03;
+
+public class Task10 {
+    public static void main(String[] args) {
+
+        int[] numbers = {5, 10, 15, 20, 25};
+
+        for(int i = numbers.length - 1; i >= 0; i--){
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
