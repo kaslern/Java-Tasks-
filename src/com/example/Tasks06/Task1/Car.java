30001ccb33b430266d3a9cdf8ae3b697d3b55b91
kaslern <adixonek@gmail.com>
2026-06-09 09:11:13 +0200

    Task1 Car class

---
+++ src/com/example/Tasks06/Task1/Car.java
@@ -0,0 +1,11 @@
+package com.example.Tasks06.Task1;
+
+public class Car {
+    String brand;
+    int yearOfProduction;
+
+    public Car(String brand, int yearOfProduction) {
+        this.brand = brand;
+        this.yearOfProduction = yearOfProduction;
+    }
+}
---
+++ src/com/example/Tasks06/Task1/Main.java
@@ -0,0 +1,10 @@
+package com.example.Tasks06.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car1 = new Car("BMW", 2021);
+
+        System.out.println(car1.brand + " " + car1.yearOfProduction);
+    }
+}
