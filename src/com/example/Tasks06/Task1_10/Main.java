8e48b160a893ee019b03146706b3be0cd5e41306
kaslern <adixonek@gmail.com>
2026-06-09 16:56:58 +0200

    Task10 array of objects

--- src/com/example/Tasks06/Task1_10/Main.java
+++ src/com/example/Tasks06/Task1_10/Main.java
@@ -9,24 +9,29 @@ public class Main {
         Car car4 = new Car("Mercedes", 2015);
         Car car5 = new Car();
 
-        System.out.println(car1.brand + " " + car1.yearOfProduction);
-        System.out.println(car2.brand + " " + car2.yearOfProduction);
-        System.out.println(car3.brand + " " + car3.yearOfProduction);
-        System.out.println(car4.brand + " " + car4.yearOfProduction);
+        //System.out.println(car1.brand + " " + car1.yearOfProduction);
+        //System.out.println(car2.brand + " " + car2.yearOfProduction);
+        //System.out.println(car3.brand + " " + car3.yearOfProduction);
+        //System.out.println(car4.brand + " " + car4.yearOfProduction);
 
-        car1.showDiffrence();
-        car1.changeBrand("Opel");
-        System.out.println(car1.brand);
+        //car1.showDiffrence();
+        //car1.changeBrand("Opel");
+        //System.out.println(car1.brand);
 
-        System.out.println(car5.brand + " " + car5.yearOfProduction);
+        //System.out.println(car5.brand + " " + car5.yearOfProduction);
 
-        car1.introduceYourself();
-        car3.introduceYourself();
+        //car1.introduceYourself();
+        //car3.introduceYourself();
 
-        Car.classDescription();
+        //Car.classDescription();
 
-        System.out.println("Number of cars created: " + Car.counter);
+        //System.out.println("Number of cars created: " + Car.counter);
 
+        Car[] cars = {car1, car2, car3};
+
+        for (Car car : cars) {
+            System.out.println(car.brand + " " + car.yearOfProduction);
+        }
 
 
     }
